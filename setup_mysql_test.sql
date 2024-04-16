9f54cf95cb13f106582da20967f90a44946556bd
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-16 11:15:08 +0300

    Task 4 MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Create a test database
+-- Creates a test user and grant permisions
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+FLUSH PRIVILEGES;
