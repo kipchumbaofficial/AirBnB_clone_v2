d1fd4367cd8f65ea61fa3f33b160e6b9ebb99056
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-05-05 17:55:45 +0300

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,37 +9,26 @@ env.key_file = '~/.ssh/id_rsa'
 env.hosts = ['107.22.144.84', '52.86.231.250']
 
 
-def do_pack():
-    '''A function to archive a directory
-    '''
-    if not os.path.exists('versions'):
-        local('mkdir versions')
-    date_time = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_name = f'web_static_{date_time}'
-    archive = local(f'tar -czvf versions/{archive_name}.tgz web_static')
-
-    if archive.succeeded:
-        return 'versions/{archive_name}'
-    else:
-        return None
-
-
 def do_deploy(archive_path):
-    '''A function to deploy archive
-    '''
+    """
+        Distribute archive.
+    """
     if os.path.exists(archive_path):
-        file_name = archive_path.split('/')[1]
-        dir_name = file_name.split('.')[0]
-        target_dir = f'/data/web_static/releases/{dir_name}'
-        put(archive_path, '/tmp/')
-        run(f'mkdir -p {target_dir}')
-        run(f'tar -xzf /tmp/{file_name} -C {target_dir}')
-        run(f'rm /tmp/{file_name}')
-        run(f'mv {target_dir}/web_static/* {target_dir}')
-        run('rm /data/web_static/current')
-        run(f'ln -s {target_dir}/ /data/web_static/current')
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
         print("New version deployed!")
-
         return True
+
     return False
