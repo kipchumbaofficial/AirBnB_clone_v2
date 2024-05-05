0424c98a4ed66051856233208bfd82aaf9d356eb
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-05-05 18:03:54 +0300

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
 '''Deploy archive
 '''
-from fabric.api import env, run, put, local
+from fabric.api import *
 import os
-from datetime import datetime
 env.user = 'ubuntu'
 env.key_file = '~/.ssh/id_rsa'
 env.hosts = ['107.22.144.84', '52.86.231.250']
 
 
 def do_deploy(archive_path):
+    """distributes an archive to your web servers
     """
-        Distribute archive.
-    """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
-
-        print("New version deployed!")
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
+        put(archive_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
-
-    return False
+    except:
+        return False
